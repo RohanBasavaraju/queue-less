389b4c71169bc94871bc888d8c6fc305f0a8099b
alexqxu <alexxu55@hotmail.com>
2020-04-25 23:26:59 -0400

    added text_section

--- q_less/lib/SplashScreen.dart
+++ q_less/lib/SplashScreen.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
+import 'sections/TextSection.dart';
 
 class SplashScreen extends StatelessWidget{
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text('QLess'),
         ),
-        body: Text('Welcome to QLess!')
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextSection(Colors.teal),
+          ],
+        )
     );
   }
 }
