6e73f5baa957d307ee238ec2c9bf832f35214a89
alexqxu <alexxu55@hotmail.com>
2020-04-26 00:30:39 -0400

    Added style.dart

--- q_less/lib/app.dart
+++ q_less/lib/app.dart
@@ -4,6 +4,13 @@ import 'SplashScreen.dart';
 class App extends StatelessWidget{
   @override
   Widget build(BuildContext context){
-    return MaterialApp(home: SplashScreen());
+    return MaterialApp(
+        home: SplashScreen(),
+        theme: ThemeData(
+          appBarTheme: AppBarTheme(
+          textTheme: TextTheme(title: AppBarTextStyle)
+        )
+      )
+    );
   }
 }
